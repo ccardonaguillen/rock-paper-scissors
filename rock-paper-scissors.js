3c936d75df6b74ff74d6c439f690449c0e63b233
ccardona <ccardonaguillen@gmail.com>
2022-08-24 21:12:47 +0200

    Create UI html and logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,13 @@
+// Game logic
+
 const options = ["Rock", "Paper", "Scissors"]
 
 function capitalise (string) {
+    // Capitalise first letter of a string
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 function getComputerChoice () {
+    // Get random draw from available options
     let randomIdx = Math.floor(Math.random() * 3);
     
     return options[randomIdx];
@@ -23,19 +27,93 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game (ngames=5) {
+/* function game (ngames=5) {
     let playerSelection;
     let computerSelection;
 
     for (let i = 0; i < ngames; i++) {
-        playerSelection = capitalise(prompt("Choose an option (rock, paper or scissors): "));
+        playerSelection =
+            capitalise(prompt("Choose an option (rock, paper or scissors): "));
 
+        // Avoid player not selecting a valid option
         while (options.indexOf(playerSelection) === -1) {
-            playerSelection = prompt("Choose an option (rock, paper or scissors): ");
+            playerSelection =
+                prompt("Choose an option (rock, paper or scissors): ");
         }
 
         computerSelection = getComputerChoice();
 
         console.log(playRound(playerSelection, computerSelection)[0]);
     }
+} */
+
+// UI logic
+
+const buttons = document.querySelectorAll('button')
+
+function connectOptionButtons() {
+    buttons.forEach(button => button.addEventListener('click', chooseOption));
 }
+
+function chooseOption(e) {
+    const playerSelection = capitalise(this.classList.value);
+    const computerSelection = getComputerChoice();
+
+    console.log(playerSelection, computerSelection);
+
+    round_score = playRound(playerSelection, computerSelection);
+    updateScore(...round_score.slice(1))
+}
+
+function updateScore(player, computer) {
+    const player_score = document.querySelector('.player-score');
+    const computer_score = document.querySelector('.computer-score');
+
+    player_score.textContent = parseInt(player_score.textContent) + player;
+    computer_score.textContent = parseInt(computer_score.textContent) + computer;
+
+    if (player_score.textContent === "5") {
+        announceWinner("PLAYER");
+    } else if (computer_score.textContent === "5") {
+        announceWinner("COMPUTER");
+    }
+}
+
+function announceWinner(winner) {
+    const announcement = document.querySelector('.winner');
+    announcement.textContent = `THE ${winner} WINS THE GAME!!!`;
+    
+    buttons.forEach(button => button.removeEventListener('click', chooseOption));
+
+    addResetButton();
+}
+
+function addResetButton() {
+    const container = document.querySelector('.announcement');
+    const reset_btn = document.createElement('button');
+    reset_btn.classList.add("reset-btn");
+    reset_btn.textContent = "Reset game";
+
+    container.appendChild(reset_btn);
+
+    reset_btn.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    const announcement = document.querySelector('.winner');
+    announcement.textContent = "";
+
+    const reset_btn = document.querySelector(".reset-btn");
+    reset_btn.remove()
+
+    const player_score = document.querySelector('.player-score');
+    const computer_score = document.querySelector('.computer-score');
+
+    player_score.textContent = 0;
+    computer_score.textContent = 0;
+
+    connectOptionButtons()
+}
+
+connectOptionButtons()
+
